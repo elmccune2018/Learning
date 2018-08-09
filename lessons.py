73993661e3f628e947d0cc1aae9b251a24f7fc55
elmccune2018 <39772815+elmccune2018@users.noreply.github.com>
2018-08-08 22:49:00 -0700

    Create lessons.py

---
+++ lessons.py
@@ -0,0 +1,13 @@
+#need to import packages
+
+"""
+From code academy, the %s allows you to assign variables in a string through string manipulation
+
+"""
+Test:
+name = raw_input("What is your name? ")
+quest = raw_input("What is your quest? ")
+color = raw_input("What is your favorite color? ")
+
+print "Ah, so your name is %s, your quest is %s, " \
+"and your favorite color is %s." % (name, quest, color)
